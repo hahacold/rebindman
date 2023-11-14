8f287007b4a0d3a30a15c7541d75d1cae2024b97
hahacold <taiwanhank0908@gmail.com>
2023-11-14 20:06:50 +0800

    Initial Commit

---
+++ bindman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bindman
+//
+//  Created by test on 2023/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bindman/bindmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  bindmanApp.swift
+//  bindman
+//
+//  Created by test on 2023/11/14.
+//
+
+import SwiftUI
+
+@main
+struct bindmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
